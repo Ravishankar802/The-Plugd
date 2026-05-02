8b80503b05647935da4abc8e188ec7c3084088ef
Ravi Shankar A <ravishankar4284@gmail.com>
2026-05-02 07:54:44 +0530

    feat: add vercel analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -44,6 +45,7 @@ export default function RootLayout({
           <div className="flex flex-col min-h-screen">
             {children}
           </div>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
