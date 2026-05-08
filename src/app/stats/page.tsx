160e17ee52e26432ef04be64829c9e50efda10bb
Ravi Shankar A <ravishankar4284@gmail.com>
2026-05-09 03:44:07 +0530

    feat: enable 60s revalidation for /stats page

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 import StatsClient from "@/components/StatsClient";
 import { cookies } from "next/headers";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 const FOLLOWERS_RANGES = [
   "0-100", "100-500", "500-1K", "1K-2K", "2K-5K", "5K-10K", "10K-25K", "25K-50K", "50K-100K", "100K+"
