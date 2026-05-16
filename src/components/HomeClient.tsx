e50fe833206539f2e2bb609f3c7598d5ce18b28a
Ravi Shankar A <ravishankar4284@gmail.com>
2026-05-17 01:50:32 +0530

    Pin footer to bottom of viewport with fixed positioning and backdrop blur

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -79,7 +79,7 @@ export default function HomeClient({
         </div>
       </div>
 
-      <div className="mt-auto w-full max-w-5xl mx-auto px-4 md:px-8 pb-10">
+      <div className="fixed bottom-0 left-0 right-0 z-[100] bg-background/50 backdrop-blur-md px-8 py-4">
         <Footer showBorder={false} />
       </div>
 
