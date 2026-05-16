0590af2663583c22842ba5268733019f7166e963
Ravi Shankar A <ravishankar4284@gmail.com>
2026-05-17 01:52:32 +0530

    Reduce footer horizontal padding from px-8 to px-4

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -79,7 +79,7 @@ export default function HomeClient({
         </div>
       </div>
 
-      <div className="fixed bottom-0 left-0 right-0 z-[100] bg-background/50 backdrop-blur-md px-8 py-4">
+      <div className="fixed bottom-0 left-0 right-0 z-[100] bg-background/50 backdrop-blur-md px-4 py-4">
         <Footer showBorder={false} />
       </div>
 
