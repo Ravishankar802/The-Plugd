8e04249ea0c57376096b3ee775be4338b43f9c12
Ravi Shankar A <ravishankar4284@gmail.com>
2026-06-13 23:09:46 +0530

    style: adjust AreaChart left margin from -20 to -5 to prevent Y-Axis label clipping on mobile

--- src/components/DashboardProfileView.tsx
+++ src/components/DashboardProfileView.tsx
@@ -1150,7 +1150,7 @@ here's my link 👉 ${link}`
                     <ResponsiveContainer width="100%" height="100%">
                       <AreaChart
                         data={processedChartData}
-                        margin={{ top: 10, right: 10, left: -20, bottom: 0 }}
+                        margin={{ top: 10, right: 10, left: -5, bottom: 0 }}
                       >
                         <defs>
                           <linearGradient id="colorEarning" x1="0" y1="0" x2="0" y2="1">
