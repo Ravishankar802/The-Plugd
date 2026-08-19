442841696c123da5c4d3296439c60b4f03c47d77
Ravi Shankar A <ravishankar4284@gmail.com>
2026-08-19 06:58:52 +0530

    remove all files

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminManageView from "@/components/AdminManageView";
-
-export const metadata = {
-  title: "Admin Panel | Plugd",
-};
-
-export default function AdminPage() {
-  return <AdminManageView />;
-}
--- src/app/admin/referrals/page.tsx
+++
@@ -1,9 +0,0 @@
-import ManageReferralsClient from "@/components/ManageReferralsClient";
-
-export const metadata = {
-  title: "Manage Referrals | Plugd Admin",
-};
-
-export default function AdminReferralsPage() {
-  return <ManageReferralsClient />;
-}
